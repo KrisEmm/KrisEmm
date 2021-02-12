6cd9a644a44e24a011425aa56a15dd04639af76b
kristian Emmanuel <kristian.emm@gmail.com>
2021-02-12 16:08:57 -0600

    Home page finished content and styles added it is running on mobil, tablet and desktop.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
 console.log("Running")
+
+const $header_menu_button = document.querySelector('#header_menu_button');
+const $menu_modal = document.querySelector('#menu_modal');
+const $menu_modal_close_button = document.querySelector("#menu_modal-close")
+const $classToShowModal = "menu-show";
+
+if ($header_menu_button) {
+    $header_menu_button.addEventListener("click", cilckHandlerMenuButton)
+}
+if ($menu_modal_close_button) {
+    $menu_modal_close_button.addEventListener("click", cilckHandlerMenuModalCloseButton)
+}
+
+function cilckHandlerMenuModalCloseButton(e) {
+    if (!$menu_modal) return;
+    $menu_modal.classList.toggle($classToShowModal);
+}
+function cilckHandlerMenuButton(e) {
+    if (!$menu_modal) return;
+    $menu_modal.classList.toggle($classToShowModal);
+}
