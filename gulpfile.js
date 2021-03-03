9b49b7b669845b028b61e76f44ac82da8a4e417a
kristian Emmanuel <kristian.emm@gmail.com>
2021-03-03 12:38:50 -0600

    The task of transpiling the project template was added

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,48 @@
 const { watch, series, src, dest } = require('gulp');
 const pug = require('gulp-pug');
+const pug_transpiler = require('pug');
 const sass = require('gulp-sass');
 const del = require('del');
+const fs = require('fs');
+const path = require('path');
 
 sass.compiler = require('sass');
 
 const data = require('./src/data/db.json')
+
+function createrHtmlFiles(repo, data) {
+    const pathTemplate = "src/pages/templates/project.pug";
+    const pathHtmlFiles = "public/";
+    const html = pug_transpiler.renderFile(
+        path.join(__dirname, pathTemplate),
+        {
+            repo,
+            data,
+            pretty: true
+        }
+    )
+    fs.appendFile(
+        path.join(__dirname, `${pathHtmlFiles}/${repo.name}.html`),
+        html,
+        function (err) {
+            if (err) throw err;
+        }
+    )
+}
 function pugTranspile(cb) {
     return src('src/pages/*.pug')
         .pipe(pug({
-            data: data,
+            locals: data,
             pretty: true
         }))
         .pipe(dest('public/'));
+}
+function pugTranspileProjectFiles(cb) {
+    if (data.repositories.length <= 0) return Promise.resolve();
+    for (const repo of data.repositories) {
+        createrHtmlFiles(repo, data)
+    }
+    return Promise.resolve();
 }
 function sassTranspile(cb) {
     return src('src/styles/**/*.scss')
@@ -39,12 +69,14 @@ function clean() {
 exports.prod = series(
     clean,
     pugTranspile,
+    pugTranspileProjectFiles,
     sassTranspile,
     assetsCopy,
 )
 exports.dev = series(
     clean,
     pugTranspile,
+    pugTranspileProjectFiles,
     sassTranspile,
     assetsCopy,
     watcher
